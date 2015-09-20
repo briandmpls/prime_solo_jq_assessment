694ab1ebdb39ab98e178b06fe8f76a410b6a9db3
briandmpls <briandmpls@gmail.com>
2015-09-20 18:12:55 -0500

    add functionality to change color on click , remove button still doesn't work

--- script.js
+++ script.js
@@ -4,62 +4,56 @@ $(document).ready(function(){
 
 var clickCounter = 0;
 
-		$(".generateButton").on("click", function(){
+				$(".generateButton").on("click", function(){
 
 
-				clickCounter ++;
-				// add class
-				$( "ul" ).addClass(".newClass");
+						clickCounter ++;
+						// add div
+						$( "body" ).append("<div class = newClass> </div>");
 
-				//create line counter text to append to HTML
-				var $newText = $("<p> Line # " + clickCounter + "</p>");
+						//create line counter text to append to HTML
+						var $newText = $("<p> Line # " + clickCounter + "</p>");
 
-				$("ul").append($newText);
-
-
-				// Create new change color button
-				var $button1 = $("<button type = button class=changeColorButton> Change Color </button>");
-				
-				$("ul").append($button1);	
-
-				//Create remove button
-
-				var $button2 = $("<button type = button class = removeButton> Remove </button>");
+						$("body").append($newText);
 
 
-				$("ul").append($button2);
+						// Create new change color button
+						var $button1 = $("<button type = button class=changeColorButton> Change Color </button>");
+						
+						$("body").append($button1);	
 
-		});
+						//Create remove button
 
-
-		// Add change color button functionality
-
-		$(".changeColorButton").on("click", ".newClass", function(){
-
-			$("this").addClass(".clicked");
-
-		});
+						var $button2 = $("<button type = button class = removeButton> Remove </button>");
 
 
+						$("body").append($button2);
+
+	
 
 		// Add remove button functionality
 
-		$(".removeButton").on("click", function(){
+				$(".removeButton").on("click", function(){
+					$(this).pre
+					console.log("test");
+				});
+
+
+
 
 			
 
 
+		// Add change color button functionality
 
+				$(".changeColorButton").on("click",function(){
+						console.log("test");
+						//$(this).prev().css("background-color", "red");
+						$(this).toggleClass("clicked");
+				});
 
+		});		
 
-
-
-
-
-
-
-
-		});
 
 });
 
